ccec409aac8f932e66d854c8923bdacdd3fb2628
swilderyude <479020008@qq.com>
2023-09-24 15:02:59 +0800

    初始化项目

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        int y = 0;
+        for (int i = 1; i <= 100; i++) {
+            y = y + i;
+        }
+        System.out.println("y = " + y);
+    }
+}
---
+++ src/com/hongyu/learnjava/Math.java
@@ -0,0 +1,4 @@
+package com.hongyu.learnjava;
+
+public class Math {
+}
